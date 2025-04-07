4254a04f7355b8940d9da9346fdae4eda0248f52
Ubirajara Junior <unjunior4315@gmail.com>
2025-04-06 23:24:00 -0300

    Bean: Testando a criptografia da senha

--- src/main/java/br/com/myhospital/Startup.java
+++ src/main/java/br/com/myhospital/Startup.java
@@ -1,13 +1,24 @@
 package br.com.myhospital;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class Startup {
+public class Startup implements CommandLineRunner {
+
+	@Autowired
+	private PasswordEncoder encoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Startup.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("senha");
+		System.out.println(encoder.encode("12345"));
+	}
 }
